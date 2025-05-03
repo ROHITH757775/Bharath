563980469d4de027fd92b191e5acc0448a2b3883
ROHITH757775 <brohith160@gmail.com>
2025-05-03 15:20:21 +0530

    Update info.py

--- info.py
+++ info.py
@@ -125,10 +125,10 @@ VERIFY_SND_SHORTLINK_API = environ.get('VERIFY_SND_SHORTLINK_API', '')
 
 
 # Shortlink Info
-SHORTLINK_MODE = bool(environ.get('SHORTLINK_MODE', False)) # Set True Or False
-SHORTLINK_URL = environ.get('SHORTLINK_URL', '')
-SHORTLINK_API = environ.get('SHORTLINK_API', '')
-TUTORIAL = environ.get('TUTORIAL', '') # How Open Shortner Link Video Link , Channel Link Where You Upload Your Video.
+SHORTLINK_MODE = bool(environ.get('SHORTLINK_MODE', True)) # Set True Or False
+SHORTLINK_URL = environ.get('SHORTLINK_URL', 'arolinks.com')
+SHORTLINK_API = environ.get('SHORTLINK_API', '2e4ffe3b84d4958b41ec436f1d8499eeb673652e')
+TUTORIAL = environ.get('TUTORIAL', 'https://t.me/+I4HSPm7DYZNkNDM1') # How Open Shortner Link Video Link , Channel Link Where You Upload Your Video.
 
 
 # Others
